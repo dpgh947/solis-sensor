9680afaa63762927bbe528fb7f70dfeb958b99f7
David Pritchard <75258259+dpgh947@users.noreply.github.com>
2026-09-01 09:21:31 +0100

    Increase api timeouts to 45
    The api is giving erratic response times atm, the 10s timeout is not sufficient. Increasing to 45 allows the calls to complete instead of producing "no valid inverters" etc.

--- custom_components/solis/soliscloud_api.py
+++ custom_components/solis/soliscloud_api.py
@@ -743,7 +743,7 @@ class SoliscloudAPI(BaseAPI):
         if self._session is None:
             return result
         try:
-            async with async_timeout.timeout(10):
+            async with async_timeout.timeout(45):
                 resp = await self._session.get(url, params=params)
 
                 result[STATUS_CODE] = resp.status
@@ -800,7 +800,7 @@ class SoliscloudAPI(BaseAPI):
         if self._session is None:
             return result
         try:
-            async with async_timeout.timeout(10):
+            async with async_timeout.timeout(45):
                 url = f"{self.config.domain}{canonicalized_resource}"
                 resp = await self._session.post(url, json=params, headers=header)
 
